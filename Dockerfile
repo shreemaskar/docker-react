be1a1d2544bb761b773984ac397287eb22d6847e
shreemaskar <shreemaskar@gmail.com>
2023-04-10 15:46:16 +0200

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16-alpine
+FROM node:16-alpine as builder
 
 USER node
 
@@ -8,4 +8,7 @@ WORKDIR /home/node/app
 COPY --chown=node:node ./package.json ./
 RUN npm install
 COPY --chown=node:node ./ ./
-CMD ["npm", "run", "start"]
+RUN npm run build
+
+FROM nginx
+COPY --from=builder /home/node/app/build /usr/share/nginx/html
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders learn react link', () => {
+  const div = document.createElement('div');
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  ReactDOM.unmountComponentAtNode(div);
 });
